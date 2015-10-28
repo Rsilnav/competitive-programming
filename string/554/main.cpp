f4b4ceba1f1fada2e76ee7dd92f692f22bdc0ce5
rafsill <rafael.silnav@gmail.com>
2015-10-28 15:46:02 +0100

    554 done.

--- string/554/main.cpp
+++ string/554/main.cpp
@@ -1,88 +1,82 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int charToInt(char c) {
+	if ((int)c == 32)
+		return 0;
+	return (int)c - 64;
+}
+
+char intToChar(int i) {
+	if (i == 0)
+		return ' ';
+	return (char)i + 64;
+}
+
 int main() {
 	ios_base::sync_with_stdio (false);
-	string diccionario[100];
-	char dic[100][20];
+	vector<string> diccionario;
 	string s;
-	int i=0;
-	while(getline(cin, s)) {
+	while (cin >> s) {
 		if (s.compare("#") == 0)
 			break;
-		diccionario[i] = s;
-		for (int x=0; x<s.size(); x++) {
-			dic[i][x] = s.at(x);
-		}
-		cout << "Pillando palabra " << i << endl;
-		i++;
+		diccionario.push_back(s);
 	}
-
-	int tamDic = i-1;
 
 	string phrase;
+	string s2;
+	int maxCount = 0;
+	string better;
+	int count;
+
+	cin.ignore();
 	getline(cin, phrase);
+	size_t found;
+	string word;
 
-	int maxWords = 0;
-	int delay;
-	int words;
-	string w;
+	for (int delay=0; delay<27; delay++) {
+		count = 0;
+		s2 = "";
+		for (int i=0; i<phrase.size(); i++) {
+			int num = charToInt(phrase.at(i));
+			num += delay;
+			num = num%27;
+			s2 += intToChar(num);
+		}
 
-
-
-	for (int l=0; l<27; l++) {
-
-		words = 0;
-
-		for (int j=0; j<tamDic; j++) {
-			w = string(dic[j]);
-			if (phrase.find(w) != string::npos) {
-				words++;
+		for (int i=0; i<diccionario.size(); i++) {
+			word = diccionario.at(i);
+			found = s2.find(word);
+			if (found != string::npos) {
+				count++;
 			}
 		}
 
-		if (words > maxWords) {
-			delay = l;
-			maxWords = words;
-		}
-
-		for (int j=0; j<tamDic; j++) {
-			for (int k=0; k<diccionario[j].size(); k++) {
-				if (dic[j][k] == ' ')
-					dic[j][k] = 'A';
-				else if (dic[j][k] == 'Z')
-					dic[j][k] = ' ';
-				else
-					dic[j][k] +=1;
-			}
+		if (count > maxCount) {
+			maxCount = count;
+			better = s2;
 		}
 
 	}
 
-	cout << "DELAY " << delay << endl;
+	int pointer = 0;
+	int sum = 0;
+	while (pointer < better.size()) {
+		if (sum == 60 || pointer == better.size()-1) {
+			while (better.at(pointer) != ' ') {
+				pointer--;
+			}
+			better = better.substr(0, pointer) + "\n" + better.substr(pointer+1);
+			pointer++;
+			sum = 0;
+		}
 
-	string result = "";
-	char cc;
-	for (int l=0; l<phrase.size(); l++) {
-		char c = phrase.at(l);
-		if (c == ' ') {
-			cc = 63 + delay;
-		}
-		else if (c+delay==91) {
-			cc = ' ';
-		}
-		else if (c+delay>91) {
-			cc = c+delay-31;
-		}
-		else {
-			cc = c + delay-2;
-		}
-		result += cc;
-
-
+		pointer++;
+		sum++;
 	}
-	cout << result << endl;
+	cout << better;
 
 	return 0;
 }
